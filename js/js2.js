f9b070d80a721d22e5d38b2bbb1c9e56d9c1a109
Valentina1425 <valentina.rodriguez1425@gmail.com>
2017-05-16 14:10:23 -0400

    añadiendo archivos

---
+++ js/js2.js
@@ -0,0 +1,26 @@
+function myFunction() {
+    alert("La pagina se ha cargado!!!!!");
+}
+
+function mouseIn(elem){
+	elem.style.border="2px solid red";
+}
+
+function mouseOut(elem){
+	elem.style.border="2px solid blue";
+}
+
+
+
+
+
+
+
+
+/*function myFunction(){
+	var textName = document.getElementById("nombre");
+		textName.value = textName.value.toUpperCase();
+}*/
+
+
+
